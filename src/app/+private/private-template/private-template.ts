f86287b528a9555ee0eac86525774edf30721e53
hadisHeydarian <hadis1319heidaryan85@gmail.com>
2025-12-13 15:17:56 +0330

    creat a guard

--- src/app/+private/private-template/private-template.ts
+++ src/app/+private/private-template/private-template.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-private-template',
@@ -8,8 +8,11 @@ import { RouterLink, RouterOutlet } from "@angular/router";
   styleUrl: './private-template.scss',
 })
 export class PrivateTemplate {
-logoff() {
-throw new Error('Method not implemented.');
-}
+  router=inject(Router);
+  logoff() {
+    sessionStorage.clear();
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/login');
+  }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { PrivateTemplate } from './+private/private-template/private-template';
 export const routes: Routes = [
     {path:'login', component:LoginPage},
     {path:'private', component:PrivateTemplate, children:[
-
+    
     ]},
     {path:'', redirectTo:'login', pathMatch:'full'},
     {path:'**', redirectTo:'login'}
