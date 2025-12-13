565245b8f296d12c80b2faf7d0912637e9541dc6
hadisHeydarian <hadis1319heidaryan85@gmail.com>
2025-12-14 00:56:00 +0330

    change font

--- src/app/+private/+pages/members-page/members-page.ts
+++ src/app/+private/+pages/members-page/members-page.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from './members-service';
+import { FormsModule } from '@angular/forms';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-members-page',
-  imports: [],
+  imports: [FormsModule,DecimalPipe],
   templateUrl: './members-page.html',
   styleUrl: './members-page.scss',
 })
-export class MembersPage {
+export class MembersPage implements OnInit{
 
+  membersService=inject(MembersService);
+  data:MembersItem[]=[];
+  action:string='list';
+  item:MembersItem={
+    id:0,
+    name:'',
+    address:'',
+    national:'',
+    phone:0,
+  }
+
+  ngOnInit(): void {
+    this.refreshData();
+  }
+  refreshData(){
+    this.data=this.membersService.list();
+  }
+  add(){
+    this.action='add';
+  }
+  cancel() {
+  this.action=='list'
+}
+save() {
+  this.membersService.add(this.item);
+  this.refreshData();
+  this.action='list';
+}
+}
+export interface MembersItem{
+  id:number;
+  name:string;
+  address:string;
+  national:string;
+  phone:number;
 }
