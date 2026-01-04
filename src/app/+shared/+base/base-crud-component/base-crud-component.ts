c19c37b82f1c23218ccc568b728e3456b0d2c140
hadisHeydarian <hadis1319heidaryan85@gmail.com>
2026-01-04 20:00:35 +0330

    creat files and base-crud-component

---
+++ src/app/+shared/+base/base-crud-component/base-crud-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-crud-component',
+  imports: [],
+  templateUrl: './base-crud-component.html',
+  styleUrl: './base-crud-component.scss',
+})
+export class BaseCrudComponent {
+
+}
