5d0970f1f3901e0d692512d805668e38221fe276
hadisHeydarian <hadis1319heidaryan85@gmail.com>
2026-01-05 01:08:48 +0330

    problme css and edit and remove

--- src/app/+private/+pages/members-page/members-service.ts
+++ src/app/+private/+pages/members-page/members-service.ts
@@ -7,11 +7,11 @@ import { BaseCRUDServise } from '../../../+shared/+base/base-service';
 })
 export class MembersService extends BaseCRUDServise<MembersItem> {
   override data: MembersItem[] = [
-    { id: 1, name: 'زهرا حسینی', address: 'همدان', national: '873664748', phone: 505330550 },
-    { id: 2, name: 'ستایش احمدی', address: 'تهران', national: '8877829928', phone: 5009829280 },
-    { id: 3, name: 'نگار میرزایی', address: 'ایلام', national: '63526883', phone: 7078262700 },
-    { id: 4, name: 'الهام ارجمند', address: 'شهرکرد', national: '8366728972', phone: 13008726260 },
-    { id: 5, name: 'حسنی مرزبان', address: 'تبریز', national: '187627287', phone: 10726527000 },
+    { id: 1, name: 'زهرا حسینی', address: 'همدان', national: '873664748', phone: '505330550' },
+    { id: 2, name: 'ستایش احمدی', address: 'تهران', national: '8877829928', phone: '5009829280' },
+    { id: 3, name: 'نگار میرزایی', address: 'ایلام', national: '63526883', phone: '7078262700'},
+    { id: 4, name: 'الهام ارجمند', address: 'شهرکرد', national: '8366728972', phone: '13008726260' },
+    { id: 5, name: 'حسنی مرزبان', address: 'تبریز', national: '187627287', phone: '10726527000' },
   ];
   override edit(destination: MembersItem, source: MembersItem): void {
     destination.name=source.name;
